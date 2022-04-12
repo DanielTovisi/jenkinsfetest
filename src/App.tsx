c9287dc1246b49a82366ae4c1fe83173ecdcd244
Daniel Tovisi <daniel.tovisi@lateral-inc.com>
2022-04-12 10:58:24 +0300

    File

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Version 1.2</p>
+        <p>Version 1.3</p>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
